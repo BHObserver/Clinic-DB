b8f9cbc900e7635a46b82bd13db1d346ebdcbc80
Kenneth Adutwum <kwamayadu@gmail.com>
2023-11-15 07:43:32 +0000

    update made in schema file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,23 +1,5 @@
 -- Create tables
-CREATE TABLE patients (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    date_of_birth DATE
-);
 
-CREATE TABLE medical_histories (
-    id SERIAL PRIMARY KEY,
-    admitted_at TIMESTAMP,
-    patient_id INT,
-    status VARCHAR,
-    FOREIGN KEY (patient_id) REFERENCES patients(id)
-);
-
-CREATE TABLE treatments (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR,
-    name VARCHAR
-);
 
 CREATE TABLE invoice_items (
     id SERIAL PRIMARY KEY,
